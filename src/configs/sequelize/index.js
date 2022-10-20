123653ac71312c46d7ceac3fcc32e18d28178304
Arief Romadhon <ariefromadhon26@gmail.com>
2022-10-20 17:36:36 +0700

    add config sequelize

--- src/configs/sequelize/index.js
+++ src/configs/sequelize/index.js
@@ -2,8 +2,12 @@ require('dotenv').config();
 
 module.exports = {
   dialect: process.env.DB_CONNECTION,
+  host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_DATABASE,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
+  define: {
+    freezeTableName: true,
+  },
 };
