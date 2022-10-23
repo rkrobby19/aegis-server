ea6b714d24da70bf3c26348a50ba163ce7101a95
Arief Romadhon <ariefromadhon26@gmail.com>
2022-10-23 19:31:05 +0700

    add dockerfile

--- src/configs/sequelize/index.js
+++ src/configs/sequelize/index.js
@@ -1,12 +1,14 @@
-require('dotenv').config();
+require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
 
 module.exports = {
+  node_env: process.env.NODE_ENV,
   dialect: process.env.DB_CONNECTION,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_DATABASE,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
+  dbSource: process.env.DB_SOURCE,
   define: {
     freezeTableName: true,
   },
