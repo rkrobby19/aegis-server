251a7c4be2b6dcb52db0116508f0a0660bf8e110
rkrobby19 <rizkyrobby19@gmail.com>
2022-10-27 17:32:48 +0700

    Add login and register api

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,6 +1,10 @@
 import UserService from '../services/user-service';
 import BaseController from './base-controller';
 
+const jwt = require('jsonwebtoken');
+const { hashSync, compareSync } = require('bcrypt');
+const { User } = require('../models');
+
 class UserController extends BaseController {
   static getUsers = async (req, res) => {
     try {
@@ -12,6 +16,75 @@ class UserController extends BaseController {
 
       return res.status(error.code).json(error);
     }
+  };
+
+  static registerUser = async (req, res) => {
+    try {
+      const {
+        username, fullname, email, password,
+      } = req.body;
+
+      const data = await User.create({
+        username,
+        fullname,
+        email,
+        password: hashSync(password, 10),
+      });
+
+      res.status(200).send({
+        message: 'success',
+        data: {
+          id: data.id,
+          username: data.username,
+          email: data.email,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(422).send({
+        message: 'Failed to create user',
+      });
+    }
+  };
+
+  static login = async (req, res) => {
+    const { username, password } = req.body;
+
+    // find user data
+    const data = await User.findOne({
+      where: {
+        username,
+      },
+    });
+
+    // user not exist
+    if (!data) {
+      return res.status(404).send({
+        message: 'data tidak ditemukan',
+      });
+    }
+
+    // user exit but wrong password
+    if (!compareSync(password, data.password)) {
+      return res.status(401).send({
+        message: 'Incorrect Password',
+      });
+    }
+
+    // oke
+    const secret = process.env.SECRET;
+    const payload = {
+      id: data.id,
+      username: data.username,
+      email: data.email,
+    };
+
+    const token = jwt.sign(payload, secret, { expiresIn: '1h' });
+
+    return res.status(200).send({
+      message: 'success',
+      token: `Bearer ${token}`,
+    });
   };
 }
 export default UserController;
--- src/index.js
+++ src/index.js
@@ -10,4 +10,6 @@ app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use('/api', api);
 
-app.listen(process.env.APP_PORT, () => { console.log(`server running at ${process.env.APP_PORT}`); });
+app.listen(process.env.APP_PORT, () => {
+  console.log(`server running at ${process.env.APP_PORT}`);
+});
