a5439d100b3b5fc08c0bb3308120619cd29a97e7
Arief Romadhon <89050998+ariefro@users.noreply.github.com>
2023-02-22 18:48:48 +0700

    Unit tests for some functions in the user service (#33)
    * add jest config
    
    * add unit test of slug to type function
    
    * add unit test for notification message function
    
    * add unit test for format rupiah function
    
    * add unit test for jwt function
    
    * add testing environment for sequelize
    
    * add unit test for getUsers function
    
    * add unit test for getUserByUsername function
    
    * add unit test for getUserByUsernameOrEmail function
    
    * add unit test for registerUser function
    
    * add unit test for loginUser function
    
    * add unit test for tokenVersionChecker function
    
    * add unit test for generateAccessToken function and generateRefreshToken function
    
    * add unit test for updateToken function
    
    * add unit test for updateTokenVersion function

--- src/configs/sequelize/index.js
+++ src/configs/sequelize/index.js
@@ -8,6 +8,13 @@ module.exports = {
     host: process.env.DB_HOST,
     dialect: process.env.DB_CONNECTION,
   },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE_TEST,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_CONNECTION,
+  },
   stage: {
     url: process.env.DB_URL,
   },
